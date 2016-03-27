6885a685ab3cb2d4f0a4dcaae2529b636b7247cf
ffarzat <fabiofarzat@gmail.com>
2016-03-27 11:36:04 -0300

    adding npm [refs #9]

--- src/NPMTester.ts
+++ src/NPMTester.ts
@@ -1,20 +1,36 @@
+import npm = require('npm');
 import ITester from './ITester';
 import IConfiguration from './IConfiguration';
 import Individual from './Individual';
+
 
  /**
  * NPMTester - executes tests for especific library
  */
 export default class NPMTester implements ITester {
     
+    config: IConfiguration;
+    
+    
+    /**
+     * Initializes NPM packages if necessary
+     */
     Setup(configuration: IConfiguration){
+        //do nothing in this case
         
+        this.config = configuration;
     }
     
     Test(individual:Individual){
         
+        //Save new code on 
+        
+        
     }
     
+    /**
+     * Backs to initial state
+     */
     Clean(){
         
     }
