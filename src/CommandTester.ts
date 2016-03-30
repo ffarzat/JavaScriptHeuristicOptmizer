eb8f25d506642bef2a93c24b848c009017280502
Andre Farzat <andrefarzat@gmail.com>
2016-03-30 00:43:13 -0300

    Fixing the test which runs the tests

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -17,7 +17,7 @@ import fs = require('fs');
  */
 export default class CommandTester implements ITester {
     config: IConfiguration;
-    
+
     //Number of times to run tests over a single individual
     testUntil: number;
     //Main file of Library
@@ -27,63 +27,60 @@ export default class CommandTester implements ITester {
     //Actual dir
     testOldDirectory:string;
     fitnessTopValue:number;
+
     /**
      * Initializes NPM packages if necessary
      */
     Setup(configuration: IConfiguration, context: OperatorContext){
-        
+
         this.config = configuration;
         this.testUntil = configuration.testUntil;
-        
-        //Setup tests with Lib context 
+
+        //Setup tests with Lib context
         var lib = context.LibrarieOverTest;
-        this.libMainFilePath = lib.mainFilePath; 
-        this.libDirectoryPath = path.join(process.cwd(), lib.path);  
+        this.libMainFilePath = lib.mainFilePath;
+        this.libDirectoryPath = path.join(process.cwd(), lib.path);
         this.testOldDirectory = process.cwd();
         this.fitnessTopValue = context.FitnessTopValue;
-        
+
         //fse.copySync(jadeLib.path, path.join(jadeLibDirectory, 'oldCode.js'));
     }
-    
+
     /**
      * Do the test for an individual
      */
-    Test(individual: Individual): number{
-              
-         var agvFit = 1;   //avg of all runs  
-        
+    Test(individual: Individual): number {
+
+        var agvFit = 1;   //avg of all runs
+
         //output new code over main file js
         this.WriteCodeToFile(individual);
-        
+
         //TODO: Try/catch
         process.chdir(this.libDirectoryPath);
 
-        var ExecutionCode = (Shell.exec(`npm test`, {silent:false}) as Shell.ExecOutputReturnValue ).code;
-        
+        var ExecutionCode = (Shell.exec('npm test', {silent:false}) as Shell.ExecOutputReturnValue).code;
+
         process.chdir(this.testOldDirectory);
-        
-        if(ExecutionCode > 0)
+
+        if (ExecutionCode > 0)
             return this.fitnessTopValue;
-            
+
         return agvFit;
     }
-    
-     /**
+
+    /**
      * Backs to initial state
      */
-    Clean(){
-        
+    Clean() {
+
     }
-    
+
     /**
      * Writes the new code Over old Main File of the lib over tests
      */
-    private WriteCodeToFile(individual: Individual)
-    {
-        fs.writeFile(this.libMainFilePath, individual.ToCode(), function (err) {
-            if (err) 
-                return console.log(err);
-        });
-    } 
-    
+    private WriteCodeToFile(individual: Individual) {
+        fs.writeFileSync(this.libMainFilePath, individual.ToCode());
+    }
+
 }
