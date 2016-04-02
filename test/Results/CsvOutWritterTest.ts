88449f67fc92aae77d782527aff7f4690af0d55c
ffarzat <fabiofarzat@gmail.com>
2016-04-02 16:34:57 -0300

    Adding the tests [closes #28]

--- test/Results/CsvOutWritterTest.ts
+++ test/Results/CsvOutWritterTest.ts
@@ -7,16 +7,31 @@ import path = require('path');
 import IConfiguration from '../../src/IConfiguration';
 import CsvResultsOutWriter from '../../src/Results/CsvResultsOutWriter';
 import IOutWriter from '../../src/IOutWriter';
+import Individual from '../../src/Individual';
 import IOutWriterFactory from '../../src/IOutWriterFactory';
+import TrialResults from '../../src/Results/TrialResults';
 
 describe('CsvOutWritterTest Tests', () => {
     
-    it('Setup write results', () => {
+    it('Should Setup write Results in Csv format', () => {
         var configurationFile: string = path.join(process.cwd(), 'test', 'Configuration.json');
         var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
 
         var concrete = new IOutWriterFactory().CreateByName(configuration.outWriter);
         concrete.Initialize(configuration);
+        var fakeResult = new TrialResults();
+        
+        fakeResult.trial = 1;
+        fakeResult.bestIndividualAvgTime = 1.69;
+        fakeResult.bestIndividualCharacters = 15968;
+        fakeResult.bestIndividualLOC = 68000;
+        
+        fakeResult.originalIndividualAvgTime = 2.1;
+        fakeResult.originalIndividualCharacters = 16000
+        fakeResult.originalIndividualLOC = 70000;
+        fakeResult.best = new Individual();
+        
+        concrete.WriteTrialResults(fakeResult);
         
         expect(concrete).not.be.an('undefined');       
     });
