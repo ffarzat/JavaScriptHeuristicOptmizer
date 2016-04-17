bdff70312ebf64c4b694689ce41843ca1c77bb75
ffarzat <fabiofarzat@gmail.com>
2016-04-17 20:09:33 -0300

    deleting Jquery lib [refs #39]

--- Libraries/jquery/src/manipulation/var/rtagName.js
+++
@@ -1,3 +0,0 @@
-define( function() {
-	return ( /<([a-z][^\/\0>\x20\t\r\n\f]+)/i );
-} );
--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,51 @@
  */
 import IConfiguration from './IConfiguration';
 import Optmizer from './Optmizer';
+import LogFactory from './LogFactory';
 
 
 import fs = require('fs');
 import path = require('path');
 import Shell = require('shelljs');
 
-//reads config
+//=========================================================================================== Reads config
 var configurationFile: string = path.join(process.cwd(), 'Configuration.json');
 var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
+var testOldDirectory: string = process.cwd();
 
-//Just prepare all libs
+//=========================================================================================== Logger
+var logger = new LogFactory().CreateByName(configuration.logWritter);
+logger.Initialize(configuration);
+
+logger.Write(`Initializing Optmizer for ${configuration.libraries.length} libraries`);
+logger.Write(`Preparing libs environment`);
+
+//=========================================================================================== Just prepare all libs
 for (var libIndex = 0; libIndex < configuration.libraries.length; libIndex++) {
     var element = configuration.libraries[libIndex];
-    
-    var returnedOutput: Shell.ExecOutputReturnValue = (Shell.exec(`cd ${element.path} && npm install`, {silent:true}) as Shell.ExecOutputReturnValue);
-    if(returnedOutput.code > 0){
-        console.log(`Library ${element.name} has error to execute npm install. It will be out of improvement process.`);
-        configuration.libraries.splice(libIndex, 1);
+    try {
+        var libDirectoryPath = path.join(process.cwd(), element.path);
+        process.chdir(libDirectoryPath);
+        var returnedOutput: Shell.ExecOutputReturnValue = (Shell.exec(`npm install`, {silent:true}) as Shell.ExecOutputReturnValue);
+
+        if(returnedOutput.code > 0){
+            logger.Write(`Library ${element.name} has error to execute npm install. It will be out of improvement process.`);
+            configuration.libraries.splice(libIndex, 1);
+        }
+        else
+        {
+            logger.Write(`Library ${element.name} instaled successfully`);
+        }
+        
+    } catch (error) {
+        logger.Write(`${error}`);
     }
-    console.log(`Library ${element.name} instaled successfully`);
+    finally{
+            process.chdir(testOldDirectory);
+    }
 }
 
-//for all trials
-var optmizer = new Optmizer();
-
+//=========================================================================================== For all trials
 for (var trial = 0; trial < configuration.trials; trial++) {
     for (var heuristicTrial = 0; heuristicTrial < configuration.trialsConfiguration.length ; heuristicTrial++) {
         var optmizer = new Optmizer();
