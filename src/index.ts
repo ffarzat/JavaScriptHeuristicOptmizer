ac2ffedb064f00967b8b47462ac19162cf3620c8
ffarzat <fabiofarzat@gmail.com>
2016-04-17 19:39:00 -0300

    running npm install over libs [refs #39]

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,23 @@ import Optmizer from './Optmizer';
 
 import fs = require('fs');
 import path = require('path');
+import Shell = require('shelljs');
 
 //reads config
 var configurationFile: string = path.join(process.cwd(), 'Configuration.json');
 var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
+
+//Just prepare all libs
+for (var libIndex = 0; libIndex < configuration.libraries.length; libIndex++) {
+    var element = configuration.libraries[libIndex];
+    
+    var returnedOutput: Shell.ExecOutputReturnValue = (Shell.exec(`cd ${element.path} && npm install`, {silent:true}) as Shell.ExecOutputReturnValue);
+    if(returnedOutput.code > 0){
+        console.log(`Library ${element.name} has error to execute npm install. It will be out of improvement process.`);
+        configuration.libraries.splice(libIndex, 1);
+    }
+    console.log(`Library ${element.name} instaled successfully`);
+}
 
 //for all trials
 var optmizer = new Optmizer();
