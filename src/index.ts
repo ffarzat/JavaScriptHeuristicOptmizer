083a84abc7ec88e86b4e7578a6e6fb0f0646487b
ffarzat <fabiofarzat@gmail.com>
2016-07-05 00:44:32 -0300

    2 secs Queue

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ logger.Initialize(configuration);
 if (cluster.isMaster) {
     localServer.logger = logger;
     localServer.Setup(configuration);
-    setInterval(function () { localServer.ProcessQueue(); }, 5000); //1x per 3 second
+    setInterval(function () { localServer.ProcessQueue(); }, 2000); //1x per 3 second
     setInterval(function () { localServer.Status(); }, 60000);
 
     var optmizerWorker = cluster.fork(); //optmizer worker
