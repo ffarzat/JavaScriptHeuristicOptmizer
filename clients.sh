509541a42c257a6dfeabcf2ca8a06b3a1c2dcc68
ffarzat <fabiofarzat@gmail.com>
2016-07-06 09:08:43 -0300

    clients script

--- clients.sh
+++ clients.sh
@@ -5,7 +5,7 @@
 
 set -x	#screen output
 #cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
-#cat - | node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js $@ | tee /mnt/scratch/user8/clients-log.txt
-cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
+cat - | node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js $@ | tee /mnt/scratch/user8/clients-log.txt
+#cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
 
 exit 0
