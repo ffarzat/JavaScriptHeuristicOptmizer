d6022a39b2f7bd0cd1398e25e2c053a64cfb8001
User test <user8@service1.smc-default.americas.sgi.com>
2016-07-06 11:52:25 +0000

    Typo

--- Scripts/clients.sh
+++ Scripts/clients.sh
@@ -4,7 +4,7 @@
 #PBS -N JavaScript
 
 set -x	#screen output
-cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
+#cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
 #cat - | node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js $@ | tee /mnt/scratch/user8/clients-log.txt
 cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
 
