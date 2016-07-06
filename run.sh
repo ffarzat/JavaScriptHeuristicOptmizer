ebd887ca723c2449abe36a538da9e8c5859e462f
ffarzat <fabiofarzat@gmail.com>
2016-07-06 10:35:28 -0300

    npm run clean

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ set -x	#screen output
 #cat - | node -v $@ | tee /mnt/scratch/user8/out.txt # test node
 echo $PBS_O_WORKDIR
 cd $PBS_O_WORKDIR
-npm start &
-#node --expose-gc --max-old-space-size=102400 build/src/index.js &
+npm run clean
+npm run build
+node --expose-gc --max-old-space-size=102400 build/src/index.js &
 node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js 
 exit 0
