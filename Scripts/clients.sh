f59842925c449dd21c3483abe8b18bca6dcf618a
ffarzat <fabiofarzat@gmail.com>
2016-07-06 08:37:32 -0300

    typo

--- Scripts/clients.sh
+++ Scripts/clients.sh
@@ -6,6 +6,6 @@
 set -x	#screen output
 cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
 #cat - | node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js $@ | tee /mnt/scratch/user8/clients-log.txt
-cat - | node build/src/teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
+cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
 
 exit 0
