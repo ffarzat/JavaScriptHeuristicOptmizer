42c2fd253e25a6bb452fff5f3bd741ea9cbdd571
ffarzat <fabiofarzat@gmail.com>
2016-07-06 09:53:49 -0300

    1 test

--- clients.sh
+++ clients.sh
@@ -10,8 +10,11 @@ set -x	#screen output
 #cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
 #cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
 
-cd /mnt/scratch/user8/JavaScriptHeuristicOptmizer
-#node build/src/Teste.js
-node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js 
+echo $PBS_O_WORKDIR
+
+cd $PBS_O_WORKDIR
+cd JavaScriptHeuristicOptmizer
+node build/src/Teste.js
+#node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js 
 
 exit 0
