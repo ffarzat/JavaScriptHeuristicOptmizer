78b85e0bd629fece96a2a368606d71bdd4f1dc78
ffarzat <fabiofarzat@gmail.com>
2016-07-06 09:29:43 -0300

    pbs err out

--- clients.sh
+++ clients.sh
@@ -2,14 +2,15 @@
 #PBS -l select=1:ncpus=48:mem=4gb
 #PBS -l place=free
 #PBS -N JavaScript
-#PBS -o /mnt/scratch/user8/clients-log.txt
-#PBS -e /mnt/scratch/user8/clients-err.txt
+#PBS -o clients-log.txt
+#PBS -e clients-err.txt
 
 set -x	#screen output
 
 #cat - | env | grep PBS $@ | tee /mnt/scratch/user8/env-vars.txt
 #cat - | node /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Teste.js $@ | tee /mnt/scratch/user8/clients-log.txt
 
-node --expose-gc --max-old-space-size=102400 /mnt/scratch/user8/JavaScriptHeuristicOptmizer/build/src/Sockets/runClients.js 
+cd JavaScriptHeuristicOptmizer
+node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js 
 
 exit 0
