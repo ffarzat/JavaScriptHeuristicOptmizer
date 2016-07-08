d35803f280c57d61e5528861ff60fcd0b32bd67a
ffarzat <fabiofarzat@gmail.com>
2016-07-08 18:56:58 -0300

    mpirun node --expose-gc --max-old-space-size=102400 build/src/Teste.js

--- src/Teste.ts
+++ src/Teste.ts
@@ -2,31 +2,7 @@
 //	node build/src/teste.js
 
 
-import IConfiguration from '../src/IConfiguration';
-import Individual from '../src/Individual';
-import ASTExplorer from '../src/ASTExplorer';
-import LogFactory from '../src/LogFactory';
-import TestResults from './TestResults';
+var numCPUs = (require('os').cpus().length);
+console.log(`[runClients] CPUS Available on host: ${numCPUs}`);
 
-import fs = require('fs');
-import path = require('path');
-
-var configurationFile: string = path.join(process.cwd(), 'Configuration.json');
-var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
-var lib = configuration.libraries[0];
-
-var logger = new LogFactory().CreateByName(configuration.logWritter);
-logger.Initialize(configuration);
-
-
-//Patch for execution over NACAD PBS 
-if (process.platform !== "win32") {
-    logger.Write(`tmpDirectory : ${configuration.tmpDirectory}`);
-    process.env['TMPDIR'] = configuration.tmpDirectory;
-}
-
-logger.Write(`process.platform : ${process.platform}`);
-logger.Write(`process.env['TMPDIR'] : ${process.env['TMPDIR']}`);
-logger.Write(`PBS_O_WORKDIR : ${process.env['PBS_O_WORKDIR']}`);
-logger.Write(`ENV : ${JSON.stringify(process.env)}`);
 
