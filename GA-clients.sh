ef2e6f49d12d0fec4bc01c305218b27c5145ebaf
ffarzat <fabiofarzat@gmail.com>
2016-07-08 18:26:53 -0300

    mpi test

--- GA-clients.sh
+++ GA-clients.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -k oe
-#PBS -l select=10:ncpus=48:mem=16gb
+#PBS -l select=10:ppn=48:mem=16gb
 #PBS -N GA-clients
 ### Request email when job begins and ends
 #PBS -m bea
@@ -11,7 +11,6 @@ date
 
 ### cd to directory where the job was submitted:
 cd $PBS_O_WORKDIR
-echo $PBS_O_WORKDIR
 
 ### determine the number of allocated processors:
 NPROCS=`wc -l < $PBS_NODEFILE`
@@ -22,11 +21,16 @@ echo "in directory:       `pwd`"
 echo "nodes: $NPROCS"
 echo "nodefile:"
 cat $PBS_NODEFILE
+NO_OF_CORES=`cat $PBS_NODEFILE | egrep -v '^#'\|'^$' | wc -l | awk '{print $1}'`
+echo "cores: $NO_OF_CORES"
 echo "----------------"
+
 
 ### run the program (on the nodes as provided by PBS):
 ### npm run PBS - o server já executa esse comando
-node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js Configs/GA-Clients.json
+#node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js Configs/GA-Clients.json
+
+mpirun -np $NO_OF_CORES -machinefile nodes node -v 
 
 date
 
