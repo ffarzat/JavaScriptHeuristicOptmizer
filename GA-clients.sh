1df287e75f30ddbdf53a0fa43c128bac8d4f7dc1
ffarzat <fabiofarzat@gmail.com>
2016-07-09 15:59:27 -0300

    #PBS -l place=group=r1i0n1

--- GA-clients.sh
+++ GA-clients.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -k oe
 #PBS -l select=1:ncpus=48:mpiprocs=48:arch=linux
-#PBS -l place=r1i0n1
+#PBS -l place=group=r1i0n1
 #PBS -N GA-clients
 ### Request email when job begins and ends
 #PBS -m bea
