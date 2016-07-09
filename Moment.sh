fa16fabf67bcfeb30b507bf15df2d554b50bcf5a
ffarzat <fabiofarzat@gmail.com>
2016-07-08 22:18:01 -0300

    Moment

---
+++ Moment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -k oe
+#PBS -l select=3:ncpus=48:mpiprocs=48
+#PBS -N Moment
+### Request email when job begins and ends
+#PBS -m bea
+### Specify email address to use for notification.
+#PBS -M ffarzat@cos.ufrj.br
+#output file
+#PBS -o my_mpi_test.out
+
+date
+
+### cd to directory where the job was submitted:
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+
+### determine the number of allocated processors:
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+echo "----------------"
+echo "PBS job running on: `hostname`"
+echo "in directory:       `pwd`"
+echo "nodes: $NPROCS"
+### echo "nodefile:"
+### cat $PBS_NODEFILE
+echo "----------------"
+
+### run the program (on the nodes as provided by PBS):
+npm run PBS
+node --expose-gc --max-old-space-size=102400 build/src/index.js Configs/Moment.json &
+mpirun -np 142 node --expose-gc --max-old-space-size=102400 build/src/Sockets/runClients.js Configs/Moment.json
+date
+
+
