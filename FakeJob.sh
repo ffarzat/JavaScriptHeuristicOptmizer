957da48435ca20632063760ac791fed649a371b8
ffarzat <fabiofarzat@gmail.com>
2016-07-10 15:10:12 -0300

    sleep 2m

--- FakeJob.sh
+++ FakeJob.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #PBS -k oe
 #PBS -l select=1:ncpus=48:mpiprocs=48:arch=linux
-#PBS -N GA-server
+#PBS -N FakeJob
 ### Request email when job begins and ends
 #PBS -m bea
 ### Specify email address to use for notification.
 #PBS -M ffarzat@cos.ufrj.br
-#output file
-#PBS -o my_mpi_test.out
 
 date
 
@@ -22,13 +20,16 @@ echo "----------------"
 echo "PBS job running on: `hostname`"
 echo "in directory:       `pwd`"
 echo "nodes: $NPROCS"
-echo "nodefile:"
-cat $PBS_NODEFILE
+### echo "nodefile:"
+### cat $PBS_NODEFILE
 echo "----------------"
 
 ### run the program (on the nodes as provided by PBS):
-npm run build
+###npm run build
 ### node --expose-gc --max-old-space-size=102400 build/src/index.js Configs/GA-Server.json
+
+sleep 2m
+
 date
 
 
