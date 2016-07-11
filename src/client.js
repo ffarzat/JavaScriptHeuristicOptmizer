fa8583dd35053e3e050efd72556fa77b9ac83425
ffarzat <fabiofarzat@gmail.com>
2016-07-11 11:05:33 -0300

    sllep for trial

--- src/client.js
+++ src/client.js
@@ -1,3 +1,6 @@
 var os = require("os");
+const child_process = require('child_process');
+
+child_process.exec(`sleep 30`);
 
 console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
