82e896500d0dcb2001b90e30a0b7a9095e293469
ffarzat <fabiofarzat@gmail.com>
2016-07-11 13:22:18 -0300

    47/5

--- src/hosts.js
+++ src/hosts.js
@@ -43,12 +43,12 @@ var async = require('async');
 
 var messagesToProcess = [];
 
-for (var i = 0; i < 1; i++) {
+for (var i = 0; i < 47; i++) {
 
     var istring = JSON.stringify(i);
     var instance = function (callback) {
         
-        var workerProcess = child_process.exec(`mpirun -np 47 --hostfile ${hostfile} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, {maxBuffer: 1024 * 5000},
+        var workerProcess = child_process.exec(`mpirun -np 5 --hostfile ${hostfile} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, {maxBuffer: 1024 * 5000},
 
             function (error, stdout, stderr) {
                 if (error) {
