9764c086ba705d7a65f45d270d34b8dc036d5305
ffarzat <fabiofarzat@gmail.com>
2016-07-11 10:55:40 -0300

    Bug

--- src/client.js
+++ src/client.js
@@ -1,3 +1,3 @@
 var os = require("os");
 
-console.log("Tests " + process.argv[2] + " executed inside host: ${os.hostname()}" );
+console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
