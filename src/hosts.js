a0ae228faa2b68434a84ab7861ff3a471b3373bb
ffarzat <fabiofarzat@gmail.com>
2016-07-11 17:24:01 -0300

    Async 5

--- src/hosts.js
+++ src/hosts.js
@@ -14,11 +14,10 @@ console.log(`hostfile: ${hostfile}`);
 var cpusString = fs.readFileSync(hostfile).toString().split("\n");
 console.log(`Ncpus == ${cpusString.length}`);
 console.log(`No host: ${cpusString[48]}`);
-console.log(`No host: ${cpusString[95]}`);
 
 //Sync
 
-
+/*
 for (var index = 0; index < 5; index++) {
 
     var uuid = require('node-uuid');
@@ -29,21 +28,23 @@ for (var index = 0; index < 5; index++) {
     var returnedOutput = Shell.exec(`mpirun -np 5 -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, { silent: false });
 }
 
-
+*/
 
 //Async
 
-/*
+
 var messagesToProcess = [];
 
-for (var i = 0; i < 46; i++) {
+for (var i = 0; i < 5; i++) {
 
     var uuid = require('node-uuid');
     var istring = uuid.v4();
 
     var instance = function (callback) {
 
-        var returnedOutput = Shell.exec(`mpirun -np 5 --hostfile ${hostfile} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, {silent:false});
+//        var returnedOutput = Shell.exec(`mpirun -np 5 --hostfile ${hostfile} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, {silent:false});
+
+        var returnedOutput = Shell.exec(`mpirun -np 5 -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, { silent: false });
     };
 
     messagesToProcess.push(instance);
@@ -59,7 +60,7 @@ async.parallel(messagesToProcess, function (err, results) {
 );
 
 
-*/
+
 
 //Parallel.Limit
 
