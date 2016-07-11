cb875cd8254f98cb4b6fe0408622463aa5ce1b0b
ffarzat <fabiofarzat@gmail.com>
2016-07-11 12:26:39 -0300

    path for npm

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ const child_process = require('child_process');
 console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
 
 
-var workerProcess = child_process.exec(`cd Libraries/uuid && npm test`, {maxBuffer: 1024 * 5000}, function (error, stdout, stderr) {
+var workerProcess = child_process.exec(`cd Libraries/uuid && /mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm test`, {maxBuffer: 1024 * 5000}, function (error, stdout, stderr) {
     if (error) {
         console.log(error.stack);
         //console.log('MPN Error code: ' + error.code);
