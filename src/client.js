5c497db54499f32a8ac03678d23b7ca5f2f0f7ab
ffarzat <fabiofarzat@gmail.com>
2016-07-11 11:17:45 -0300

    more testing

--- src/client.js
+++ src/client.js
@@ -1,6 +1,18 @@
 var os = require("os");
 const child_process = require('child_process');
 
-child_process.exec(`cd Libraries/moment && npm test`);
+var workerProcess = child_process.exec(`cd Libraries/moment && npm test`, function (error, stdout, stderr) {
+    if (error) {
+        console.log(error.stack);
+        console.log('MPN Error code: ' + error.code);
+        console.log('MPNSignal received: ' + error.signal);
+    }
+    console.log('MPN stdout: ' + stdout);
+    console.log('MPNstderr: ' + stderr);
+});
+
+workerProcess.on('exit', function (code) {
+    console.log('MPN process exited with exit code ' + code);
+}););
 
 console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
