74d6c405b75f641ba06787fb3d5ea66196cb83fe
ffarzat <fabiofarzat@gmail.com>
2016-07-11 17:25:59 -0300

    10

--- src/hosts.js
+++ src/hosts.js
@@ -35,16 +35,15 @@ for (var index = 0; index < 5; index++) {
 
 var messagesToProcess = [];
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 10; i++) {
 
     var uuid = require('node-uuid');
-    var istring = uuid.v4();
 
     var instance = function (callback) {
 
 //        var returnedOutput = Shell.exec(`mpirun -np 5 --hostfile ${hostfile} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, {silent:false});
 
-        var returnedOutput = Shell.exec(`mpirun -np 5 -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${istring}`, { silent: false });
+        var returnedOutput = Shell.exec(`mpirun -np 5 -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${uuid.v4()}`, { silent: false });
     };
 
     messagesToProcess.push(instance);
