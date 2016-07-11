56d2cdd3d79b7efbb6ee09bc664587c21ba2430b
ffarzat <fabiofarzat@gmail.com>
2016-07-11 09:43:41 -0300

    uuid

--- src/hosts.js
+++ src/hosts.js
@@ -10,7 +10,7 @@ for (var i = 0; i < 4; i++) {
 
     var instance = function (callback) {
         var os = require("os");
-        var returnedOutput = Shell.exec('cd Libraries/uuid && npm test', {silent:true});
+        var returnedOutput = Shell.exec('cd Libraries/uuid && npm test', {silent:false});
         console.log(`Hostname: ${os.hostname()} - Tests done `);
     };
 
