bbbccf013f52a522ae93eebb8db0bbd65e33ea71
ffarzat <fabiofarzat@gmail.com>
2016-07-11 11:10:52 -0300

    Testing for real

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 var os = require("os");
 const child_process = require('child_process');
 
-child_process.exec(`sleep 30`);
+child_process.exec(`cd Libraries/uuid && npm test`);
 
 console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
