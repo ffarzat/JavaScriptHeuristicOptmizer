5463bdbacf8175777e49d0f84c2a90fa18756c5c
ffarzat <fabiofarzat@gmail.com>
2016-07-11 11:15:03 -0300

    moment  trials

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 var os = require("os");
 const child_process = require('child_process');
 
-child_process.exec(`cd Libraries/uuid && npm test`);
+child_process.exec(`cd Libraries/moment && npm test`);
 
 console.log(`Tests ${process.argv[2]} executed inside host: ${os.hostname()}`);
