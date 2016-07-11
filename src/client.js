2772c8fe4c55727b02e20c60812612413cf8f944
ffarzat <fabiofarzat@gmail.com>
2016-07-11 11:56:54 -0300

    no console

--- src/client.js
+++ src/client.js
@@ -4,11 +4,11 @@ const child_process = require('child_process');
 var workerProcess = child_process.exec(`cd Libraries/moment && npm test`, {maxBuffer: 1024 * 5000}, function (error, stdout, stderr) {
     if (error) {
         console.log(error.stack);
-        console.log('MPN Error code: ' + error.code);
-        console.log('MPNSignal received: ' + error.signal);
+        //console.log('MPN Error code: ' + error.code);
+        //console.log('MPNSignal received: ' + error.signal);
     }
-    console.log('MPN stdout: ' + stdout);
-    console.log('MPNstderr: ' + stderr);
+    //console.log('MPN stdout: ' + stdout);
+    //console.log('MPNstderr: ' + stderr);
 });
 
 workerProcess.on('exit', function (code) {
