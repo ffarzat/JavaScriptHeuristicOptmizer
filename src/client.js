c9d37dd60e8472cc172e4924087a33760bb3dfc1
ffarzat <fabiofarzat@gmail.com>
2016-07-11 12:53:05 -0300

    out

--- src/client.js
+++ src/client.js
@@ -19,8 +19,8 @@ var workerProcess = child_process.exec(`
             //console.log('MPN Error code: ' + error.code);
             //console.log('MPNSignal received: ' + error.signal);
         }
-        //console.log('MPN stdout: ' + stdout);
-        //console.log('MPNstderr: ' + stderr);
+        console.log('MPN stdout: ' + stdout);
+        console.log('MPNstderr: ' + stderr);
     });
 
 workerProcess.on('exit', function (code) {
