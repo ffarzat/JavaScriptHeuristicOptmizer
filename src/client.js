492ca8021a893f6d61dbc58b8d56790e8fe19227
ffarzat <fabiofarzat@gmail.com>
2016-07-12 00:15:43 -0300

    ai ai ai...

--- src/client.js
+++ src/client.js
@@ -18,12 +18,12 @@ var workerProcess = child_process.exec(`cd Libraries/${libPath} && npm test`, { 
         //console.log(error.stack);
         //console.log('MPN Error code: ' + error.code);
         //console.log('MPNSignal received: ' + error.signal);
-        console.log(`{id:"${id}", sucess:"false", host:"${os.hostname()}", duration:"${clock(start)}"}`);
+        console.log(`{"id":"${id}", "sucess":"false", "host":"${os.hostname()}", "duration":"${clock(start)}"}`);
         process.exit(error.code);
     }
     //console.log('MPN stdout: ' + stdout);
     //console.log('MPNstderr: ' + stderr);
-    console.log(`{id:"${id}", sucess:"true", host:"${os.hostname()}", duration:"${clock(start)}"}`);
+    console.log(`{"id":"${id}", "sucess":"true", "host:"${os.hostname()}", "duration":"${clock(start)}"}`);
     process.exit();
 });
 
