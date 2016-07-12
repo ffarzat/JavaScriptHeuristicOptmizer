12fb0fcc069aec9a3feb04fcd08651af96d4f2b5
ffarzat <fabiofarzat@gmail.com>
2016-07-12 11:03:33 -0300

    No Debug msg

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -147,7 +147,8 @@ export default class CommandTester implements ITester {
             /* FOR MPIRUN using */
 
         } catch (error) {
-            this.logger.Write(error);
+            //this.logger.Write(error);
+            this.logger.Write(`[CommandTester] Tests Failed.`);
             passedAllTests = false;
         }
         finally {
