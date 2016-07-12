771b2258752cdc4b0bbae305b9123a7c1e815c07
ffarzat <fabiofarzat@gmail.com>
2016-07-12 07:55:48 -0300

    testUntil config

--- src/hosts.js
+++ src/hosts.js
@@ -20,6 +20,7 @@ console.log(`Test Host #1: ${cpusString[48]}`);
 //var clientsTotal = 9;
 var libname = "moment";
 var timeoutMS = 15000;
+var testUntil = 5;
 
 //Async
 
@@ -29,7 +30,7 @@ var actualHost = cpusString[48];
 
 try {
     var msgId = uuid.v4();
-    var stdout = child_process.execSync(`mpirun -np 5 -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libname} ${timeoutMS}`, { maxBuffer: 1024 * 5000 }).toString();
+    var stdout = child_process.execSync(`mpirun -np ${testUntil} -host ${cpusString[48]} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libname} ${timeoutMS}`, { maxBuffer: 1024 * 5000 }).toString();
 
     var stringList = stdout.replace(/(?:\r\n|\r|\n)/g, ',');;
     stringList = stringList.substring(0, stringList.length - 1);
