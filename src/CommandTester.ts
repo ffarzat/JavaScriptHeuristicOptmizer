4a11a62b659ffada6fa8e993474233d6f9bb9d91
ffarzat <fabiofarzat@gmail.com>
2016-07-14 19:01:55 -0300

    Debug message for AvailableHosts.length

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -50,7 +50,9 @@ export default class CommandTester implements ITester {
         this.testUntil = testUntil;
         this.testTimeout = testTimeout;
         this.AvailableHosts = Hosts;
-        console.log(`[CommandTester] Hosts: ${this.AvailableHosts.length}`);
+        
+        if(AvailableHosts)
+            console.log(`[CommandTester] Hosts: ${this.AvailableHosts.length}`);
         
         //Setup tests with Lib context
         this.libMainFilePath = LibrarieOverTest.mainFilePath;
