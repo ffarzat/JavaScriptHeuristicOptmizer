a43a444d77139ad2011b7de8fe4cdf13b543ae9c
ffarzat <fabiofarzat@gmail.com>
2016-07-15 18:45:09 -0300

    Refactoring for parallel testing

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -113,7 +113,7 @@ export default class CommandTester implements ITester {
             var bufferOption = { maxBuffer: 1024 * 5000 }
 
             if (this.AvailableHosts == undefined || this.AvailableHosts.length == 0) {
-                testCMD = `node --expose-gc --max-old-space-size=2047 src/client.js ${msgId} ${libPath} ${timeoutMS}`;
+                testCMD = `node --expose-gc --max-old-space-size=2047 build/src/MPI/client.js ${libPath} ${timeoutMS}`;
                 bufferOption = {maxBuffer: 200*1024};
             }
             else {
@@ -126,7 +126,7 @@ export default class CommandTester implements ITester {
                 
                 hosts = hosts.substring(0, hosts.length - 1);
 
-                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libPath} ${timeoutMS}`;
+                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 build/src/MPI/client.js ${libPath} ${timeoutMS}`;
 
 
                 //console.log(`[CommandTester] Hosts count: ${this.AvailableHosts.length}`);
--- src/Teste.ts
+++ src/Teste.ts
@@ -1,207 +1,23 @@
 /// <reference path="./typings/tsd.d.ts" />
-//	node build/src/teste.js
-/*
-import fs = require('fs');
-import path = require('path');
-import expect = require('expect.js');
-
-import IConfiguration from './IConfiguration';
-import ASTExplorer from './ASTExplorer';
-import Individual from './Individual';
-import OperatorContext from './OperatorContext';
-import Message from './Sockets/Message';
-import LogFactory from './LogFactory';
-
-var uuid = require('node-uuid');
-var pool = require('fork-pool');
-var async = require('async');
-
-var numCPUs = (require('os').cpus().length);
-
-
-/**
- * Just for test messages
- 
-var configFile = process.argv[2] != undefined ? process.argv[2] : 'Configuration.json';
-var configurationFile: string = path.join(process.cwd(), configFile);
-
-var Ncpus = process.argv[3];
-var hostfile = process.argv[4];
-var clientOptions = '--max-old-space-size=512000';
-var allHosts: Array<string> = [];
-var allHostsList = fs.readFileSync(hostfile).toString().split("\n");
-
-allHostsList.forEach(element => {
-
-    if (allHosts.indexOf(element) == -1 && element != "") {
-        allHosts.push(element);
-    }
-});
-
-allHosts.splice(0, 1); //removing actual host
-
-console.log(`Hosts available:`);
-allHosts.forEach(element => {
-    console.log(`-> ${element}`);
-});
-
-
-var astExplorer: ASTExplorer = new ASTExplorer();
-var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
-var lib = configuration.libraries[0];
-var libFile: string = lib.mainFilePath;
-var generatedIndividual: Individual = astExplorer.GenerateFromFile(libFile);
-var logger = new LogFactory().CreateByName(configuration.logWritter);
-logger.Initialize(configuration);
-
-
-//Patch for execution over NACAD PBS 
-if (process.platform !== "win32") {
-    process.env['TMPDIR'] = configuration.tmpDirectory;
-}
-
-
-//console.log(`CPUS Available: ${numCPUs}`);
-console.log(`LIB: ${lib.name}`);
-
-//var indexes: number[] = astExplorer.IndexNodes(generatedIndividual);
-
-var FirstMsg: Message = new Message();
-var context = new OperatorContext();
-context.Operation = "StartClients";
-context.First = generatedIndividual;
-context.Original = generatedIndividual; //is usual to be the original
-context.LibrarieOverTest = lib;
-
-FirstMsg.ActualLibrary = lib.name;
-FirstMsg.ctx = context;
-
-//========================================================================================== Clients Pool
-var uniquePool = new pool(__dirname + '/Child.js', [configFile], { execArgv: [clientOptions] }, { size: configuration.clientsTotal + 1, log: false, timeout: configuration.copyFileTimeout * 1000 });
-
-/*
-uniquePool.enqueue(JSON.stringify(FirstMsg), (err, obj) => {
-    doBegin();
-});
-
-
-var messageList: Array<Message> = [];
-
-
-//function doBegin() {
-var messagesToProcess = [];
-
-for (var i = 0; i < configuration.trialsConfiguration[0].especific.neighborsToProcess; i++) {
-
-    var msg: Message = new Message();
-    var context = new OperatorContext();
-    context.Operation = "Test";
-    context.First = generatedIndividual;
-    context.Original = generatedIndividual;
-    context.LibrarieOverTest = lib;
-
-    msg.Hosts = allHosts;
-    msg.ActualLibrary = lib.name;
-    msg.ctx = context;
-    msg.id = i;
-
-    messageList.push(msg);
-
-    /*
-    var instance = function (callback) {
-        uniquePool.enqueue(JSON.stringify(messageList.pop()), callback);
-    };
-
-    messagesToProcess.push(instance);
-    
-}
-
-logger.Write(`Total clients ${configuration.clientsTotal}`);
-logger.Write(`Total messages ${messageList.length}`);
-
-var totalProcessedMsgs = 0;
-var exectimer = require('exectimer');
-var Tick = new exectimer.Tick(5000);
-Tick.start();
-
-messageList.forEach(element => {
-    logger.Write(`Sending message ${element.id}`);
-
-    uniquePool.enqueue(JSON.stringify(element), (err, obj) => {
-        totalProcessedMsgs++;
-        
-        if (err)
-            logger.Write(`err: ${err.stack}`);
-
-
-        var processedMessage = obj.stdout;
-        logger.Write(`msg ${processedMessage.id} done.`);
-
-        if (totalProcessedMsgs == configuration.trialsConfiguration[0].especific.neighborsToProcess) {
-            Tick.stop();
-            var trialTimer = exectimer.timers[5000];
-            logger.Write(`Total time: ${ToNanosecondsToSeconds(trialTimer.duration())}`);
-
-            uniquePool.drain(function (err) {
-                if (err)
-                    logger.Write(`err: ${err.stack}`);
-
-                process.exit();
-            });
-
-        }
-    });
-});
-
-logger.Write(`All messages were sent.`);
-
-
-
-
-/*
-Tick.stop();
-var trialTimer = exectimer.timers[1];
-console.log(`Tempo: ${ToNanosecondsToSeconds(trialTimer.duration())}`);
-
-async.parallel(messagesToProcess,
-    function (err, results) {
-        if (err)
-            console.log(`err: ${err.stack}`);
-
-        console.log(`results: ${results.length}`);
-
-        Tick.stop();
-        var trialTimer = exectimer.timers[1];
-        console.log(`Tempo: ${ToNanosecondsToSeconds(trialTimer.duration())}`);
-
-        process.exit();
-
-    }
-);
-*/
-//}
-
-/**
- * Transform nano secs in secs
- 
-function ToNanosecondsToSeconds(nanovalue: number): number {
-    return parseFloat((nanovalue / 1000000000.0).toFixed(3));
-}
-
-*/
 
 import ASTExplorer from './ASTExplorer';
 import IConfiguration from './IConfiguration';
 import Individual from './Individual';
 import OperatorContext from './OperatorContext';
 import LogFactory from './LogFactory';
-import Queue from './MPI/Queue';
-const child_process = require('child_process');
+//import Queue from './MPI/Queue';
+import TestResults from './TestResults';
+
+
+import child_process = require('child_process');
 import path = require('path');
 import fs = require('fs');
+var async = require("async");
 
 // create a new queue
-var fila = new Queue();
+//var Mensagens = new Queue();
+var totalSlots = configuration.clientsTotal;
+var messagesToProcess = [];
 
 //Reads the config
 var configFile = process.argv[2] != undefined ? process.argv[2] : 'Configuration.json';
@@ -252,18 +68,11 @@ context.First = generatedIndividual;
 context.Original = generatedIndividual; //is usual to be the original
 context.LibrarieOverTest = lib;
 
-var testUntil = 5;
+var testUntil = configuration.testUntil;
 var timeoutMS = configuration.clientTimeout * 1000;
 var libPath = "Libraries/uuid";
 
-var hosts: string = "";
-allHosts.forEach(host => {
-    hosts += `${host},`;
-});
 
-hosts = hosts.substring(0, hosts.length - 1);
-
-var testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 build/src/MPI/client.js ${libPath} ${timeoutMS}`;
 
 
 var passedAllTests = true;
@@ -273,34 +82,122 @@ var avg;
 var median;
 
 
+for (var index = 0; index < configuration.trialsConfiguration[0].especific.neighborsToProcess; index++) {
 
-var stdout = child_process.execSync(testCMD, bufferOption).toString();
-//console.log(`[CommandTester] After`);
-var stringList = stdout.replace(/(?:\r\n|\r|\n)/g, ',');;
-stringList = stringList.substring(0, stringList.length - 1);
-//console.log(`[${stringList}]`);
+    var instance = function (callback) {
+        var mutante = astExplorer.Mutate(context);
 
-var list = JSON.parse(`[${stringList}]`);
-var numbers = [];
-for (var index = 0; index < list.length; index++) {
-    var element = list[index];
-    numbers.push(element.duration);
-    //this.logger.Write(`${stdout}`)
-    console.log(`[Executado no host: ${element.host}:${element.duration}/${element.sucess}]`);
+        var contextMutante = new OperatorContext();
+        contextMutante.Operation = "StartClients";
+        contextMutante.First = mutante;
+        contextMutante.Original = generatedIndividual; //is usual to be the original
+        contextMutante.LibrarieOverTest = lib;
+
+        var slotFree = GetFreeSlot();
+        var directoryToTest = configuration.tmpDirectory + `/${slotFree}` + contextMutante.LibrarieOverTest.name
+        logger.Write(`Testing... ${directoryToTest}`);
+        
+        Testar(contextMutante.LibrarieOverTest.mainFilePath, contextMutante.First, directoryToTest, timeoutMS, allHosts);
+        callback();
+    }
+
+    messagesToProcess.push(instance);
 }
 
-max = Math.max.apply(null, numbers);
-min = Math.min.apply(null, numbers);
-avg = CalculateMean(numbers);
-median = CalculateMedian(numbers);
-passedAllTests = (list[0].sucess === "true");
 
-console.log(`Min: ${ToSecs(min)}`);
-console.log(`Max: ${ToSecs(max)}`);
-console.log(`Mean: ${ToSecs(avg)}`);
-console.log(`Median: ${ToSecs(median)}`);
-console.log(`Duration: ${ToSecs(max)}`); // Now is Max
-console.log(`passedAllTests: ${passedAllTests}`); // Now is Max
+
+
+async.parallelLimit(messagesToProcess, configuration.clientsTotal, function(error, results) {
+  console.log(error);
+  console.log(results);
+});
+//==================================================================================================================//>
+
+function GetFreeSlot(): number{
+    var actual = totalSlots;
+    totalSlots--;
+    return actual;
+}
+
+
+function Testar(libMainFilePath: string, mutant: Individual, npmCmdDir: string, timeout: number, allHosts: any) {
+    var hosts: string = "";
+    var testCMD = "";
+
+
+    var passedAllTests = true;
+
+    var max;
+    var min;
+    var avg;
+    var median;
+
+
+    if (allHosts) {
+        allHosts.forEach(host => {
+            hosts += `${host},`;
+        });
+
+        hosts = hosts.substring(0, hosts.length - 1);
+
+        testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 build/src/MPI/client.js ${npmCmdDir} ${timeout}`;
+    }
+    else {
+        testCMD = `node --expose-gc --max-old-space-size=2047 src/client.js ${npmCmdDir} ${timeout}`;
+        bufferOption = { maxBuffer: 200 * 1024 };
+    }
+
+    try {
+
+        WriteCodeToFile(mutant, libMainFilePath);
+
+        var stdout = child_process.execSync(testCMD, bufferOption).toString();
+        //console.log(`[CommandTester] After`);
+        var stringList = stdout.replace(/(?:\r\n|\r|\n)/g, ',');;
+        stringList = stringList.substring(0, stringList.length - 1);
+        //console.log(`[${stringList}]`);
+
+        var list = JSON.parse(`[${stringList}]`);
+        var numbers = [];
+        for (var index = 0; index < list.length; index++) {
+            var element = list[index];
+            numbers.push(element.duration);
+            //this.logger.Write(`${stdout}`)
+            console.log(`{${element.host}:${element.duration}:${element.sucess}`);
+        }
+
+        max = Math.max.apply(null, numbers);
+        min = Math.min.apply(null, numbers);
+        avg = CalculateMean(numbers);
+        median = CalculateMedian(numbers);
+        passedAllTests = (list[0].sucess === "true");
+
+    } catch (error) {
+        logger.Write(`${error.stack}`);
+
+        max = 0;
+        min = 0;
+        avg = 0;
+        median = 0;
+        passedAllTests = false;
+    }
+
+
+
+    console.log(`Min: ${ToSecs(min)}`);
+    console.log(`Max: ${ToSecs(max)}`);
+    console.log(`Mean: ${ToSecs(avg)}`);
+    console.log(`Median: ${ToSecs(median)}`);
+    console.log(`Duration: ${ToSecs(max)}`); // Now is Max
+}
+
+/**
+ * Writes the new code Over old Main File of the lib over tests
+ */
+function WriteCodeToFile(individual: Individual, libMainFilePath: string) {
+    //this.logger.Write(`Saving over file ${this.libMainFilePath}`);
+    fs.writeFileSync(libMainFilePath, individual.ToCode());
+}
 
 
 /**
@@ -314,7 +211,7 @@ function ToSecs(number) {
 /**
  * simple median
  */
-function CalculateMedian(values):number {
+function CalculateMedian(values): number {
 
     values.sort(function (a, b) { return a - b; });
 
@@ -329,7 +226,7 @@ function CalculateMedian(values):number {
 /**
  * simple mean
  */
-function CalculateMean(numbers):number {
+function CalculateMean(numbers): number {
     var total = 0
     for (var i = 0; i < numbers.length; i++) {
         total = total + numbers[i];
