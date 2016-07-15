5c8298f24f759327004ca61f930b51374f44ed96
ffarzat <fabiofarzat@gmail.com>
2016-07-15 11:01:58 -0300

    echo 'será?'

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -129,7 +129,7 @@ export default class CommandTester implements ITester {
 
                 //testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libPath} ${timeoutMS}`;
 
-                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm cd ${libPath} && npm test`;
+                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm cd ${libPath} && npm test && echo 'será?'`;
 
                 //console.log(`[CommandTester] Hosts count: ${this.AvailableHosts.length}`);
                 //console.log(`[CommandTester] cmd: ${testCMD}`);                
