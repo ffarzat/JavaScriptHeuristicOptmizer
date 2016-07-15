cb2ca3a5b526e4dd97cdf64948224f57ddb4776d
ffarzat <fabiofarzat@gmail.com>
2016-07-15 15:22:12 -0300

    Debug

--- src/Teste.ts
+++ src/Teste.ts
@@ -198,13 +198,13 @@ var workerProcess = child_process.exec(`mpirun -n 5 -host r2i4n10.ib0.smc-defaul
 
     if (error) {
         console.log(error.stack);
-        console.log('MPN Error code: ' + error.code);
-        console.log('MPNSignal received: ' + error.signal);
+        //console.log('MPN Error code: ' + error.code);
+        //console.log('MPNSignal received: ' + error.signal);
         //console.log(`{"id":"${id}", "sucess":"false", "host":"${os.hostname()}", "duration":"${clock(start)}"}`);
         process.exit(error.code);
     }
-    console.log('MPN stdout: ' + stdout);
-    console.log('MPNstderr: ' + stderr);
+    //console.log('MPN stdout: ' + stdout);
+    //console.log('MPNstderr: ' + stderr);
     //console.log(`{"id":"${id}", "sucess":"true", "host":"${os.hostname()}", "duration":"${clock(start)}"}`);
     process.exit();
 });
