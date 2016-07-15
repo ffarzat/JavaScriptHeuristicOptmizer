1cd03802714975762bf25ed3bba90051e715f866
ffarzat <fabiofarzat@gmail.com>
2016-07-15 13:59:20 -0300

    -x PBS_GET_IBWINS=1

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -126,7 +126,7 @@ export default class CommandTester implements ITester {
                 
                 hosts = hosts.substring(0, hosts.length - 1);
 
-                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x $PBS_GET_IBWINS=1,PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libPath} ${timeoutMS}`;
+                testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${msgId} ${libPath} ${timeoutMS}`;
 
 
                 //console.log(`[CommandTester] Hosts count: ${this.AvailableHosts.length}`);
