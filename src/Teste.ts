741498b63bd9ec316e615b0efe641f90cf0a4c44
ffarzat <fabiofarzat@gmail.com>
2016-07-15 16:54:36 -0300

    MPI/

--- src/Teste.ts
+++ src/Teste.ts
@@ -263,7 +263,7 @@ allHosts.forEach(host => {
 
 hosts = hosts.substring(0, hosts.length - 1);
 
-var testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 src/client.js ${libPath} ${timeoutMS}`;
+var testCMD = `mpirun -n ${testUntil} -host ${hosts} -x PBS_GET_IBWINS=1 -x PATH=$PATH:node=/mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node:npm=/mnt/scratch/user8/nodev4/node-v4.4.7/out/bin/npm /mnt/scratch/user8/nodev4/node-v4.4.7/out/Release/node --expose-gc --max-old-space-size=102400 MPI/client.js ${libPath} ${timeoutMS}`;
 
 
 var passedAllTests = true;
