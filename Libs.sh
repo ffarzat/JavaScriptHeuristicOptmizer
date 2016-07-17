62fb6ba4d4c8791b84f07814f132ef7e2c7c5bef
ffarzat <fabiofarzat@gmail.com>
2016-07-17 19:45:18 -0300

    Libs actual trials

--- Libs.sh
+++ Libs.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 
-### Exectimer		
-	qsub Exectimer/exectimer-0.sh
-	qsub Exectimer/exectimer-10.sh
-	qsub Exectimer/exectimer-20.sh		
-	qsub Exectimer/exectimer-30.sh		
-	qsub Exectimer/exectimer-40.sh		
-	qsub Exectimer/exectimer-50.sh
+### Exectimer
+	echo "Exectimer/exectimer-0.sh" && 	qsub Exectimer/exectimer-0.sh
+	echo "Exectimer/exectimer-10.sh" && qsub Exectimer/exectimer-10.sh
+	echo "Exectimer/exectimer-20.sh" && qsub Exectimer/exectimer-20.sh		
+	echo "Exectimer/exectimer-30.sh" && qsub Exectimer/exectimer-30.sh		
+	echo "Exectimer/exectimer-40.sh" && qsub Exectimer/exectimer-40.sh		
+	echo "Exectimer/exectimer-50.sh" && qsub Exectimer/exectimer-50.sh
 
 ### Express
-	qsub Express/Express-0.sh
-	qsub Express/Express-10.sh
-	qsub Express/Express-20.sh 			
-	qsub Express/Express-30.sh	
-	qsub Express/Express-40.sh
-	qsub Express/Express-50.sh
+	echo "Express/Express-0.sh" && qsub Express/Express-0.sh
+	echo "Express/Express-10.sh" && qsub Express/Express-10.sh
+	echo "Express/Express-20.sh" && qsub Express/Express-20.sh 			
+	echo "Express/Express-30.sh" && qsub Express/Express-30.sh	
+	echo "Express/Express-40.sh" && qsub Express/Express-40.sh
+	echo "Express/Express-50.sh" && qsub Express/Express-50.sh
 
 ### Jade			
-    qsub Jade/Jade-20.sh
-    qsub Jade/Jade-30.sh
-	qsub Jade/Jade-40.sh
-	qsub Jade/Jade-50.sh
+    echo "Jade/Jade-20.sh" && qsub Jade/Jade-20.sh
+    echo "Jade/Jade-30.sh" && qsub Jade/Jade-30.sh
+	echo "Jade/Jade-40.sh" && qsub Jade/Jade-40.sh
+	echo "qsub Jade/Jade-50.sh" && qsub Jade/Jade-50.sh
 
 ### Gulp
-	qsub Gulp/RD-gulp-cccr.sh
-	qsub Gulp/GA-0-gulp-cccr.sh
-	qsub Gulp/GA-10-gulp-cccr.sh
-	qsub Gulp/GA-20-gulp-cccr.sh	
-	qsub Gulp/GA-30-gulp-cccr.sh
-	qsub Gulp/GA-40-gulp-cccr.sh
-	qsub Gulp/GA-50-gulp-cccr.sh
+	echo "Gulp/RD-gulp-cccr.sh" && qsub Gulp/RD-gulp-cccr.sh
+	echo "Gulp/GA-0-gulp-cccr.sh" && qsub Gulp/GA-0-gulp-cccr.sh
+	echo "Gulp/GA-10-gulp-cccr.sh" && qsub Gulp/GA-10-gulp-cccr.sh
+	echo "Gulp/GA-20-gulp-cccr.sh" && qsub Gulp/GA-20-gulp-cccr.sh	
+	echo "Gulp/GA-30-gulp-cccr.sh" && qsub Gulp/GA-30-gulp-cccr.sh
+	echo "Gulp/GA-40-gulp-cccr.sh" && qsub Gulp/GA-40-gulp-cccr.sh
+	echo "Gulp/GA-50-gulp-cccr.sh" && qsub Gulp/GA-50-gulp-cccr.sh
 
 ### Lodash		
-	qsub Lodash/Lodash-0.sh
-	qsub Lodash/Lodash-20.sh
-	qsub Lodash/Lodash-30.sh
-	qsub Lodash/Lodash-40.sh
-	qsub Lodash/Lodash-50.sh
+	echo "Lodash/Lodash-0.sh" && qsub Lodash/Lodash-0.sh
+	echo "Lodash/Lodash-20.sh" && qsub Lodash/Lodash-20.sh
+	echo "Lodash/Lodash-30.sh" && qsub Lodash/Lodash-30.sh
+	echo "Lodash/Lodash-40.sh" && qsub Lodash/Lodash-40.sh
+	echo "Lodash/Lodash-50.sh" && qsub Lodash/Lodash-50.sh
 
 ### Node-Browserify
-    qsub Node-browserrify/Job0.sh
-	qsub Node-browserrify/Job40.sh
-    qsub Node-browserrify/Job50.sh
+    echo "Node-browserrify/Job0.sh" && qsub Node-browserrify/Job50.sh
