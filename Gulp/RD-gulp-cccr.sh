aa3221cf21a38c43db71e598d84b58b6c5f4447b
ffarzat <fabiofarzat@gmail.com>
2016-07-17 14:09:46 -0300

    RD trials

--- Gulp/RD-gulp-cccr.sh
+++ Gulp/RD-gulp-cccr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -k oe
 #PBS -l select=1:ncpus=48
-#PBS -N gulp-ccr-50
+#PBS -N RD-gulp-ccr
 ### Request email when job begins and ends
 #PBS -m bea
 ### Specify email address to use for notification.
