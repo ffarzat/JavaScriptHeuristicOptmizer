248892fbf00ebccbcedc6365a5917963e2548ce3
ffarzat <fabiofarzat@gmail.com>
2016-08-21 11:13:03 -0300

    Moment HC trials

---
+++ Moment/run.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#PBS -k oe
+#PBS -l select=1:ncpus=48
+#PBS -N Moment
+#PBS -m bea
+#PBS -M ffarzat@cos.ufrj.br
+cd $PBS_O_WORKDIR
+echo "----------------"
+echo "PBS job running on: `hostname`"
+echo "in directory:       `pwd`"
+echo "nodes: $NPROCS"
+echo "----------------"
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 0
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 1
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 2
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 3
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 4
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 5
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 6
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 7
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 8
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 9
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 10
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 11
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 12
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 13
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 14
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 15
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 16
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 17
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 18
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 19
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 20
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 21
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 22
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 23
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 24
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 25
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 26
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 27
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 28
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 29
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 30
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 31
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 32
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 33
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 34
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 35
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 36
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 37
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 38
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 39
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 40
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 41
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 42
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 43
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 44
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 45
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 46
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 47
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 48
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 49
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 50
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 51
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 52
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 53
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 54
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 55
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 56
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 57
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 58
+
+node --max-old-space-size=2408000 build/src/index.js Configuration/moment.json null null 59
+
--- stringPBS.js
+++ stringPBS.js
@@ -1,38 +1,34 @@
-var Libname = "Jquery";
-var ConfigName = "Jquery/jquery.json";
-var DirectoryToSave = "Jquery";
+var Libname = "Moment";
+var ConfigName = "Configuration/moment.json";
+var DirectoryToSave = "Moment";
 //====================================================================================//>
 var fs = require('fs');
-var runResult = "";
-runResult += "#!/bin/bash" + "\n";
+//var runResult = "";
+//runResult += "#!/bin/bash" + "\n";
+
+var result = "";
+result += "#!/bin/bash" + "\n";
+result += "#PBS -k oe" + "\n";
+result += "#PBS -l select=1:ncpus=48" + "\n";
+result += `#PBS -N ${Libname}` + "\n";
+result += "#PBS -m bea" + "\n";
+result += "#PBS -M ffarzat@cos.ufrj.br" + "\n";
+result += "cd $PBS_O_WORKDIR" + "\n";
+result += 'echo "----------------"' + "\n";
+result += 'echo "PBS job running on: `hostname`"' + "\n";
+result += 'echo "in directory:       `pwd`"' + "\n";
+result += 'echo "nodes: $NPROCS"' + "\n";
+result += 'echo "----------------"' + "\n";
 
 for (var index = 0; index < 60; index++) {
 
-    var result = "";
-    result += "#!/bin/bash" + "\n";
-    result += "#PBS -k oe" + "\n";
-    result += "#PBS -l select=1:ncpus=48" + "\n";
-    result += `#PBS -N ${Libname}-${index}` + "\n";
-    result += "#PBS -m bea" + "\n";
-    result += "#PBS -M ffarzat@cos.ufrj.br" + "\n";
-    result += "cd $PBS_O_WORKDIR" + "\n";
-    result += 'echo "----------------"' + "\n";
-    result += 'echo "PBS job running on: `hostname`"' + "\n";
-    result += 'echo "in directory:       `pwd`"' + "\n";
-    result += 'echo "nodes: $NPROCS"' + "\n";
-    result += 'echo "----------------"' + "\n";
     result += `node --max-old-space-size=2408000 build/src/index.js ${ConfigName} null null ${index}\n` + "\n";
-
-
-    runResult += `qsub ${DirectoryToSave}/${Libname}-${index}.sh` + "\n";
-    runResult += `sleep 5m` + "\n";
-
 
     if (!fs.existsSync(DirectoryToSave)) {
         fs.mkdirSync(DirectoryToSave);
     }
 
-    fs.writeFileSync(`${DirectoryToSave}/${Libname}-${index}.sh`, result);
+    //fs.writeFileSync(`${DirectoryToSave}/${Libname}-${index}.sh`, result);
 }
 
-fs.writeFileSync(`${DirectoryToSave}/run.sh`, runResult);
+fs.writeFileSync(`${DirectoryToSave}/run.sh`, result);
