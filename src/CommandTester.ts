8b1940c5c009ed7a3fe407ba8008683b6c03385d
ffarzat <fabiofarzat@gmail.com>
2016-12-27 12:33:27 -0200

    Debug para testes

--- src/CommandTester.ts
+++ src/CommandTester.ts
@@ -129,6 +129,8 @@ export default class CommandTester implements ITester {
                 //console.log(`[CommandTester] cmd: ${testCMD}`);                
             }
 
+            console.log(`[CommandTester] cmd: ${testCMD}`);                
+
             //console.log(`[CommandTester] Before`);
             var stdout = child_process.execSync(testCMD, bufferOption).toString();
             //console.log(`[CommandTester] After`);
