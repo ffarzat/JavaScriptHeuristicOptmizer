d43c73922f908816728cb5f536c801e910a14f3d
Fabio <fabiofarzat@gmail.com>
2017-02-25 10:45:45 -0300

    #PBS -l walltime=500:00:00

--- Lodash/run.20.sh
+++ Lodash/run.20.sh
@@ -2,6 +2,7 @@
 #PBS -k oe
 #PBS -l select=1:ncpus=48
 #PBS -N Lodash-20
+#PBS -l walltime=500:00:00
 #PBS -m bea
 #PBS -M ffarzat@cos.ufrj.br
 cd $PBS_O_WORKDIR
--- uuid/run.10.sh
+++ uuid/run.10.sh
@@ -2,6 +2,7 @@
 #PBS -k oe
 #PBS -l select=1:ncpus=48
 #PBS -N uuid-10
+#PBS -l walltime=500:00:00
 #PBS -m bea
 #PBS -M ffarzat@cos.ufrj.br
 cd $PBS_O_WORKDIR
