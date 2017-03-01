7e58e61408659d7883a274057d9541d1f0ba6d12
Fabio <fabiofarzat@gmail.com>
2017-03-01 09:54:45 -0300

    lodahs testes

--- test/CommandTesterTest.ts
+++ test/CommandTesterTest.ts
@@ -56,7 +56,7 @@ describe('CommandTester Tests', function () {
 
         var configurationFile: string = path.join(process.cwd(), 'test', 'Configuration.json');
         var configuration: IConfiguration = JSON.parse(fs.readFileSync(configurationFile, 'utf8'));
-        var lib = configuration.libraries[4]; //Libraries/pug
+        var lib = configuration.libraries[3]; //Libraries/lodash
 
         //Creates the Inidividual for tests
         var astExplorer: ASTExplorer = new ASTExplorer();
