f1eaff9a8d34111af5d9a2fd9b2240feb9fd4b56
Fabio <fabiofarzat@gmail.com>
2017-03-09 09:14:10 -0300

    Analise de tempo de execução

---
+++ ExecutarTempo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+date
+node build/src/AnaliseTempo.js '/home/fabio/Github/JavaScriptHeuristicOptmizer/Libraries/uuid' 'lib/uuid.js' '/home/fabio/Dropbox/Doutorado/2017/Experimentos/Funcao Dinamica/uuid' 30 20
+date
+node build/src/AnaliseTempo.js '/home/fabio/Github/JavaScriptHeuristicOptmizer/Libraries/pug' 'packages/pug/lib/index.js' '/home/fabio/Dropbox/Doutorado/2017/Experimentos/Funcao Dinamica/pug' 30 20
+date
