0ce48e441df98f182eb4ddae251934ad3195884b
Fabio <fabiofarzat@gmail.com>
2017-03-10 07:52:36 -0300

    Trocando a analise de tempo de global para Função

--- src/AnaliseTempo.ts
+++ src/AnaliseTempo.ts
@@ -3,6 +3,7 @@
 //node build/src/AnaliseTempo.js '/home/fabio/Github/JavaScriptHeuristicOptmizer/Libraries/uuid' 'lib/uuid.js' '/home/fabio/Dropbox/Doutorado/2017/Experimentos/Funcao Dinamica/uuid' 30 20
 //node build/src/AnaliseTempo.js '/home/fabio/Github/JavaScriptHeuristicOptmizer/Libraries/pug' 'packages/pug/lib/index.js' '/home/fabio/Dropbox/Doutorado/2017/Experimentos/Funcao Dinamica/pug' 30 20
 
+import ASTExplorer from './ASTExplorer';
 import TestResults from './TestResults';
 
 
@@ -40,67 +41,90 @@ var oldLibFilePath = path.join(DiretorioBiblioteca, 'old.js');
 if (!fs.existsSync(oldLibFilePath))
     fse.copySync(arquivoRootBiblioteca, oldLibFilePath, { "clobber": true });
 
+Executar();
 
 //============================================================================================ Original //>
+async function Executar() {
+    var caminhoOriginal = DiretorioResultados + `/${heuristicas[0]}/original.js`;
+    
+    //instrumentar, executar, salvar
+    
+    var codigoOriginal = fs.readFileSync(caminhoOriginal, 'UTF8');
+    WriteCodeToFile(arquivoRootBiblioteca, codigoOriginal);
 
-var caminhoOriginal = DiretorioResultados + `/${heuristicas[0]}/original.js`;
-var codigoOriginal = fs.readFileSync(caminhoOriginal, 'UTF8');
-WriteCodeToFile(arquivoRootBiblioteca, codigoOriginal);
+    //Despreza a primeira execuçao
+    await ExecutarTeste(DiretorioBiblioteca, bufferOption, 1);
+    var resultadoOriginal = await ExecutarTeste(DiretorioBiblioteca, bufferOption, Quantidade);
 
-//Despreza a primeira execuçao
-ExecutarTeste(DiretorioBiblioteca, bufferOption, 1, "0", "original")
-var resultadoOriginal = ExecutarTeste(DiretorioBiblioteca, bufferOption, Quantidade, "0", "original");
-resultadosProcessados.push(resultadoOriginal);
 
-//============================================================================================ Rodadas //>
+    resultadoOriginal.Trial = "0";
+    resultadoOriginal.Heuristic = "original";
+    resultadosProcessados.push(resultadoOriginal);
 
-heuristicas.forEach(heuristica => {
-    for (var index = 0; index < QuantidadeRodadas; index++) {
-        console.log(`Executando rodada ${index} da heuristica ${heuristica}`);
+    //============================================================================================ Rodadas //>
 
-        var caminhoArquivoRodada = DiretorioResultados + "/" + heuristica + "/" + index + ".js";
-        if (!fs.existsSync(caminhoArquivoRodada))
-        {
-            console.log(`Arquivo ${caminhoArquivoRodada} não existe!`);
-            continue;
-        }
+    for(var j=0; j < heuristicas.length; j++)
+    {
+        var heuristica = heuristicas[j];
+        
+        for (var index = 0; index < QuantidadeRodadas; index++) {
 
-        var CodigoDaRodada = fs.readFileSync(caminhoArquivoRodada, 'UTF8');
+            console.log(`Executando rodada ${index} da heuristica ${heuristica}`);
 
-        if (CodigoDaRodada != codigoOriginal) {
+            var caminhoArquivoRodada = DiretorioResultados + "/" + heuristica + "/" + index + ".js";
+            if (!fs.existsSync(caminhoArquivoRodada)) {
+                console.log(`Arquivo ${caminhoArquivoRodada} não existe!`);
+                continue;
+            }
 
-            WriteCodeToFile(arquivoRootBiblioteca, CodigoDaRodada);
+            var CodigoDaRodada = fs.readFileSync(caminhoArquivoRodada, 'UTF8');
 
-            resultadosProcessados.push(ExecutarTeste(DiretorioBiblioteca, bufferOption, Quantidade, `${index}`, heuristica));
-        }
-        else {
-            console.log(`   O código é igual ao original`);
-            var resultadoFinal: TestResults = new TestResults();
+            if (CodigoDaRodada != codigoOriginal) {
 
-            resultadoFinal.rounds = Quantidade;
-            resultadoFinal.min = resultadoOriginal.min;
-            resultadoFinal.max = resultadoOriginal.max;
-            resultadoFinal.mean = resultadoOriginal.mean;
-            resultadoFinal.median = resultadoOriginal.median;
-            resultadoFinal.duration = resultadoOriginal.duration;
-            resultadoFinal.passedAllTests = true;
+                WriteCodeToFile(arquivoRootBiblioteca, CodigoDaRodada);
 
-            resultadoFinal.Trial = "0";
+                //Instrumenta, executa os testes, avalia
 
-            resultadosProcessados.push();
+                var resultadosLocal = await ExecutarTeste(DiretorioBiblioteca, bufferOption, Quantidade);
+                
+                resultadosLocal.Trial = `${index}`;
+                resultadosLocal.Heuristic = heuristica;
+                
+                resultadosProcessados.push(resultadosLocal);
+            }
+            else {
+                console.log(`   O código é igual ao original`);
+                var resultadoFinal: TestResults = new TestResults();
+
+                resultadoFinal.rounds = Quantidade;
+                resultadoFinal.min = resultadoOriginal.min;
+                resultadoFinal.max = resultadoOriginal.max;
+                resultadoFinal.mean = resultadoOriginal.mean;
+                resultadoFinal.median = resultadoOriginal.median;
+                resultadoFinal.duration = resultadoOriginal.duration;
+                resultadoFinal.passedAllTests = true;
+
+                resultadoFinal.Trial = "0";
+
+                resultadosProcessados.push();
+            }
         }
     }
-});
 
-//============================================================================================ Fim //>
-//Volta a cópia de segurança
-fse.copySync(oldLibFilePath, arquivoRootBiblioteca, { "clobber": true });
+    //============================================================================================ Fim //>
+    //Volta a cópia de segurança
+    fse.copySync(oldLibFilePath, arquivoRootBiblioteca, { "clobber": true });
 
-console.log(`Escrever csv com ${resultadosProcessados.length} resultados obtidos`);
+    console.log(`Escrever csv com ${resultadosProcessados.length} resultados obtidos`);
 
-EscreverResultadoEmCsv(DiretorioResultados, resultadosProcessados);
+    EscreverResultadoEmCsv(DiretorioResultados, resultadosProcessados);
 
-process.exit();
+    process.exit();
+}
+
+
+
+
 
 
 /**
@@ -113,27 +137,30 @@ function WriteCodeToFile(caminho: string, codigo: string) {
 /**
  * Executa os testes e recupera os valores
  */
-function ExecutarTeste(DiretorioBiblioteca: string, bufferOption: any, quantidade: number, trial: string, heuristica: string): TestResults {
+async function ExecutarTeste(DiretorioBiblioteca: string, bufferOption: any, quantidade: number): Promise<TestResults> {
     var msgId = uuid.v4();
     var passedAllTests = true;
 
-    var testCMD = `node --expose-gc --max-old-space-size=512000  src/client.js 0 ${DiretorioBiblioteca} ${120000}`; //timeout bem grande
-
-    if (os.hostname() != "Optmus") {
-        child_process.execSync("sleep 1", bufferOption).toString();
-    }
+    var testCMD = `node --expose-gc --max-old-space-size=2047 'src/client.js' ${msgId} ${DiretorioBiblioteca} 3600000`;
 
     var stdout = "";
+    var durations = [];
+    var start = process.hrtime();
 
     for (var index = 0; index < quantidade; index++) {
 
         try {
-            var Tick = new exectimer.Tick(msgId);
-            Tick.start();
             console.log(`   ${index}x`);
 
-            stdout = child_process.execSync(testCMD, bufferOption).toString();
-            Tick.stop();
+            var promessaDoBem = new Promise<string>((resolve, reject) => {
+                var processo = child_process.exec(testCMD, bufferOption, (erro: Error, stdout: string, stderr: string) => {
+                    processo.kill();
+                    erro ? reject(erro) : resolve(stdout);
+                });
+
+            });
+
+            stdout = await promessaDoBem;
 
             var stringList = stdout.replace(/(?:\r\n|\r|\n)/g, ',');;
             stringList = stringList.substring(0, stringList.length - 1);
@@ -144,35 +171,64 @@ function ExecutarTeste(DiretorioBiblioteca: string, bufferOption: any, quantidad
                 passedAllTests = false;
                 break;
             }
+
         } catch (error) {
             console.log(`testCMD: ${testCMD}`);
             console.log(`stdout: ${stdout}`);
             console.log(`${error.stack}`);
 
-            Tick.stop();
+            //Tick.stop();
             passedAllTests = false;
             break;
+        }
+        finally {
+            durations.push(clock(start));
         }
 
     }
 
-    var unitTestsTimer = exectimer.timers[msgId];
+    //var unitTestsTimer = exectimer.timers[msgId];
     var resultadoFinal: TestResults = new TestResults();
 
+    var sum = arraySUM(durations);
+    var avg = sum / durations.length;
+
     resultadoFinal.rounds = quantidade;
-    resultadoFinal.min = ToNanosecondsToSeconds(unitTestsTimer.min());
-    resultadoFinal.max = ToNanosecondsToSeconds(unitTestsTimer.max());
-    resultadoFinal.mean = ToNanosecondsToSeconds(unitTestsTimer.mean());
-    resultadoFinal.median = ToNanosecondsToSeconds(unitTestsTimer.median());
-    resultadoFinal.duration = ToNanosecondsToSeconds(unitTestsTimer.duration());
-    resultadoFinal.passedAllTests = passedAllTests
-
-    resultadoFinal.Trial = trial;
-    resultadoFinal.Heuristic = heuristica;
-
-    //ShowConsoleResults(resultadoFinal);
+    resultadoFinal.min = arrayMin(durations);
+    resultadoFinal.max = arrayMax(durations);
+    resultadoFinal.mean = avg;
+    resultadoFinal.median = getMedian(durations);
+    resultadoFinal.duration = sum;
+    resultadoFinal.rounds = quantidade;
+    resultadoFinal.passedAllTests = passedAllTests;
 
     return resultadoFinal;
+}
+
+/**
+ * Retorna a lista de funções de um arquivo Js
+ * @param caminhoOriginal 
+ */
+function ExtrairListaDeFuncoes(caminhoOriginal: string) {
+    var caminho = __dirname.replace('build/', '');
+    var functionExtractor = require(caminho + '/heuristics/function-extractor.js');
+    var astExplorer = new ASTExplorer();
+    var individuo = astExplorer.GenerateFromFile(caminhoOriginal);
+    var lista = functionExtractor.interpret(individuo.AST);
+    var listaApenasComNomes = [];
+
+    lista.forEach(element => {
+        listaApenasComNomes.indexOf(element.name == -1)
+        listaApenasComNomes.push(element.name);
+    });
+
+    var unique = listaApenasComNomes.filter(function (elem, index, self) {
+        return index == self.indexOf(elem);
+    })
+
+    //console.log(JSON.stringify(unique));
+
+    return unique;
 
 }
 
@@ -227,4 +283,49 @@ function parseMillisecondsIntoReadableTime(milliseconds) {
     var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
 
     return h + ':' + m + ':' + s;
+}
+
+//1 second is equal to 1000000000 nanoseconds, or 1000 milliseconds.
+function clock(startTime): number {
+    var end = process.hrtime(startTime);
+    const convertHrtime = require('convert-hrtime');
+    var resultado = convertHrtime(end);
+    return resultado.ms;
+}
+
+function arrayMin(arr) {
+    var len = arr.length, min = Infinity;
+    while (len--) {
+        if (parseFloat(arr[len]) < min) {
+            min = arr[len];
+        }
+    }
+    return min;
+};
+
+function arrayMax(arr) {
+    var len = arr.length, max = -Infinity;
+    while (len--) {
+        if (parseFloat(arr[len]) > max) {
+            max = arr[len];
+        }
+    }
+    return max;
+};
+
+function arraySUM(arr) {
+    var len = arr.length;
+    var total = 0;
+    while (len--) {
+        total += parseFloat(arr[len]);
+    }
+    return total;
+};
+
+function getMedian(args) {
+    if (!args.length) { return 0 };
+    var numbers = args.slice(0).sort((a, b) => a - b);
+    var middle = Math.floor(numbers.length / 2);
+    var isEven = numbers.length % 2 === 0;
+    return isEven ? (numbers[middle] + numbers[middle - 1]) / 2 : numbers[middle];
 }
