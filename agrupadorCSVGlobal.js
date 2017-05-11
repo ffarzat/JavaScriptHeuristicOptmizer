ed819c8a6d381cf2b502bd325844dc36fecadda3
Fabio <fabiofarzat@gmail.com>
2017-05-11 19:46:03 -0300

    Agrupador dos resultados globais

--- agrupadorCSVGlobal.js
+++ agrupadorCSVGlobal.js
@@ -1,19 +1,36 @@
-var Directory = "/home/fabio/Dropbox/Doutorado/2017/Experimentos/Funcao Estatica/uuid/RD";
-var FileResults = "Results-grouped.csv";
-//====================================================================================//>
 var fs = require('fs');
+var path = require('path');
+
+
+var DiretorioDosResultados = process.argv[2].replace("'", "");
+var FileResults = path.join(DiretorioDosResultados, "Results-grouped.csv");
+//====================================================================================//>
+
+var ListaDasBibliotecas = getDirectories(DiretorioDosResultados)
 var runResult = "";
-runResult += "sep=,\ntrial,originalIndividualAvgTime,originalIndividualLOC,originalIndividualCharacters,bestIndividualAvgTime,bestIndividualLOC,bestIndividualCharacters,time,better\n";
+runResult += "Lib;Heuristic;Trial;Lines;% Improved Loc;Chars;% Improved Chars;Time Spent\n";
 
-for (var index = 0; index < 30; index++) {
+ListaDasBibliotecas.forEach(function (biblioteca) {
+    var arquivoBiblioteca = path.join(DiretorioDosResultados, biblioteca, "analiseTempoExecucao.csv");
 
-    var text = fs.readFileSync(`${Directory}/${index}-Results.csv`,'utf8');
-    var arr = text.split("\n");  
-    //console.log(arr[2]);
-    
-    runResult += `${arr[2]}\n`;
+    var text = fs.readFileSync(arquivoBiblioteca, 'utf8');
+    var arr = text.split("\n");
+
+    for (var index = 1; index < arr.length - 1; index++) {
+        runResult += `${arr[index]}\n`;
+    }
+
+}, this);
+
+console.log(`Generated File: ${FileResults}`);
+
+fs.writeFileSync(FileResults, runResult);
+
+
+//==================================================================
+
+
+function getDirectories(srcpath) {
+    return fs.readdirSync(srcpath)
+        .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
 }
-
-console.log(`Generated File: ${Directory}/${FileResults}`);
-
-fs.writeFileSync(`${Directory}/${FileResults}`, runResult);
