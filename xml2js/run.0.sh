2da1990618a8721251ad5e851532d71a5a12999e
Fabio Farzat <fabiofarzat@gmail.com>
2017-06-03 22:12:22 -0300

    xml2js RD

---
+++ xml2js/run.0.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#PBS -k oe
+#PBS -l select=1:ncpus=48
+#PBS -N xml2js-0
+#PBS -l walltime=500:00:00
+#PBS -m bea
+#PBS -M ffarzat@cos.ufrj.br
+cd $PBS_O_WORKDIR
+echo "----------------"
+echo "PBS job running on: `hostname`"
+echo "in directory:       `pwd`"
+echo "nodes: $NPROCS"
+echo "----------------"
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 0
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 1
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 2
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 3
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 4
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 5
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 6
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 7
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 8
+
+node --max-old-space-size=2408000 build/src/index.js xml2js/xml2js.json null null 9
