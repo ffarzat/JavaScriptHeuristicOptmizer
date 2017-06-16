63c057f9005bd9d94336459faaea9a7fc65df7c3
Fabio Farzat <fabiofarzat@gmail.com>
2017-06-15 23:17:38 -0300

    node-browserify RD

--- src/GerarPlanilhaGlobal.ts
+++ src/GerarPlanilhaGlobal.ts
@@ -31,7 +31,7 @@ var uglifyOptions = {
     }
 };
 
-var heuristicas = ['HC3', 'HC4', 'HC5'];
+var heuristicas = ['RD', 'HC3', 'HC4', 'HC5'];
 var DiretorioBiblioteca = process.argv[2].replace("'", "");
 var arquivoRootBiblioteca = process.argv[3].replace("'", "");
 var DiretorioResultados = process.argv[4].replace("'", "");
