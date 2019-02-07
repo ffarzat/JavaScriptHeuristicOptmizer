3eb99ba2d87b5a4dceaf45ee1d8cc25188e845cf
Fabio Farzat <fabiofarzat@gmail.com>
2019-02-07 12:35:57 -0200

    node-semver rodada 4

--- node-semver/run.0.sh
+++ node-semver/run.0.sh
@@ -11,13 +11,13 @@ echo "PBS job running on: `hostname`"
 echo "in directory:       `pwd`"
 echo "nodes: $NPROCS"
 echo "----------------"
-node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 0
+#node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 0
 
-node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 1
+#node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 1
 
-node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 2
+#node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 2
 
-node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 3
+#node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 3
 
 node --max-old-space-size=2408000 build/src/index.js node-semver/node-semver.json null null 4
 
